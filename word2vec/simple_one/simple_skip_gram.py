a7f0ab0123cbe145cab430aa4245afab1c48f792
“david“ <david@largitdata.com>
2020-08-15 08:59:43 +0800

    add simple

---
+++ word2vec/simple_one/simple_skip_gram.py
@@ -0,0 +1,46 @@
+﻿# coding: utf-8
+import sys
+sys.path.append('..')
+import numpy as np
+from common.layers import MatMul, SoftmaxWithLoss
+
+
+class SimpleSkipGram:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+
+        # 初始化權重
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(H, V).astype('f')
+
+        # 產生各層
+        self.in_layer = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer1 = SoftmaxWithLoss()
+        self.loss_layer2 = SoftmaxWithLoss()
+
+        # 把全部的權重與梯度整合成清單
+        layers = [self.in_layer, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # 在成員變數中，設定字詞的分式散表示
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        h = self.in_layer.forward(target)
+        s = self.out_layer.forward(h)
+        l1 = self.loss_layer1.forward(s, contexts[:, 0])
+        l2 = self.loss_layer2.forward(s, contexts[:, 1])
+        loss = l1 + l2
+        return loss
+
+    def backward(self, dout=1):
+        dl1 = self.loss_layer1.backward(dout)
+        dl2 = self.loss_layer2.backward(dout)
+        ds = dl1 + dl2
+        dh = self.out_layer.backward(ds)
+        self.in_layer.backward(dh)
+        return None
